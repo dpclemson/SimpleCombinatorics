51ca52eabfc0f82efe997d7935dad0f50348f3bd
Dylan Clemson <dpclemso@uncg.edu>
2021-10-26 14:47:36 -0400

    Added Condor Output and plotting utility

--- OHist.C
+++ OHist.C
@@ -9,7 +9,7 @@ void OHist(){
   callHK(2,8);
 }
 void callHK(int H,int K){
-  TFile* file = TFile::Open(Form("OutputFiles/OutFile_h%d_k%d.root",H,K));
+  TFile* file = TFile::Open(Form("CondorOutput/Combined_h%d_k%d.root",H,K));
   if ( file == NULL ){
     cout << "Warning Missing File!"<< endl;
     return;
@@ -19,10 +19,17 @@ void callHK(int H,int K){
     cout << "Warning Histogram Missing!" << endl;
     return;
   }
+  gStyle->SetOptStat(0);
+  gStyle->SetOptTitle(0);
+  gStyle->SetPadTickX(1);
+  gStyle->SetPadTickY(1);
   TCanvas c1("c1","");
   histogram->SetMarkerStyle(kFullCircle);
   histogram->SetMarkerColor(kBlack);
   histogram->Draw("ex0p");
+  histogram->GetXaxis()->SetTitle("N_{particles}");
+  histogram->GetYaxis()->SetTitle(Form("#LT%d#GT_{%d}",K,H));
   c1.SetLogy();
+  c1.SetGrid(1);
   c1.Print(Form("Figures/TestHist_h%d_k%d.png",H,K));
 }
